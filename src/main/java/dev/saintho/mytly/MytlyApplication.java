6f80800bdd23845d794fd7e78a99e6d1363ec8a7
toneofrain <toneofrain@gmail.com>
2023-04-27 17:10:36 +0900

    chore: 프로젝트 생성 (#2)

---
+++ src/main/java/dev/saintho/mytly/MytlyApplication.java
@@ -0,0 +1,13 @@
+package dev.saintho.mytly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/saintho/mytly/MytlyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saintho.mytly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
