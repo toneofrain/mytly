398540144688ec490517420db832113d5791efc9
toneofrain <toneofrain@gmail.com>
2023-04-30 19:17:40 +0900

    feat: 단축 URL 삭제 서비스 작성 (#19)

--- src/main/java/dev/saintho/mytly/exception/ExceptionType.java
+++ src/main/java/dev/saintho/mytly/exception/ExceptionType.java
@@ -1,5 +1,7 @@
 package dev.saintho.mytly.exception;
 
+import static org.springframework.http.HttpStatus.*;
+
 import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
@@ -8,7 +10,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ExceptionType {
+	URL_NOT_FOUND(NOT_FOUND, "URL_NOT_FOUND");
 	;
 	private final HttpStatus status;
-	private String message;
+	private final String message;
 }
--- src/main/java/dev/saintho/mytly/repository/UrlRepository.java
+++ src/main/java/dev/saintho/mytly/repository/UrlRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import dev.saintho.mytly.entity.Url;
 
 public interface UrlRepository extends JpaRepository<Url, Long> {
+	Optional<Url> findByShortened(String shortenend);
+
 	Optional<Url> findByOriginal(String original);
 
 	boolean existsByShortened(String shortened);
