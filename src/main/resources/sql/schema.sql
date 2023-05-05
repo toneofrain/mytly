2e438631e1704b3f2497bbcb9faf347a8a9e05f5
toneofrain <toneofrain@gmail.com>
2023-05-05 09:06:55 +0900

    chore: update daily_engagement index

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -17,7 +17,8 @@ CREATE TABLE url (
     expirable_yn TINYINT(1) NOT NULL DEFAULT 0,
     expire_at TIMESTAMP,
     PRIMARY KEY (id),
-    UNIQUE INDEX ux_shortened (shortened)
+    UNIQUE INDEX ux_shortened (shortened),
+    INDEX ix_original (original)
 );
 
 CREATE TABLE referer_engagement (
@@ -33,7 +34,8 @@ CREATE TABLE daily_engagement (
     url_id BIGINT UNSIGNED NOT NULL,
     engage_date DATE NOT NULL,
     engage_count MEDIUMINT UNSIGNED NOT NULL DEFAULT 1,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    INDEX ix_url_id_engage_date (url_id, engage_date)
 );
 
 ALTER TABLE referer_engagement
