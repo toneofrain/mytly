b7648bbe1d02166d29ad7176f9497b988f1bcd41
toneofrain <toneofrain@gmail.com>
2023-05-06 12:34:24 +0900

    fix: Enum

--- src/main/java/dev/saintho/mytly/api/v1/urls/dto/command/UrlShortCommand.java
+++ src/main/java/dev/saintho/mytly/api/v1/urls/dto/command/UrlShortCommand.java
@@ -2,6 +2,7 @@ package dev.saintho.mytly.api.v1.urls.dto.command;
 
 import java.time.LocalDateTime;
 
+import dev.saintho.mytly.api.v1.urls.dto.request.ExpirationPeriod;
 import dev.saintho.mytly.api.v1.urls.dto.request.UrlPostRequest;
 import lombok.Getter;
 
@@ -9,17 +10,21 @@ import lombok.Getter;
 public class UrlShortCommand {
 	private String original;
 	private Boolean isExpirable;
+	private ExpirationPeriod expirationPeriod;
 	private LocalDateTime requestedAt;
-	private LocalDateTime expireAt;
 
 	public static UrlShortCommand of(UrlPostRequest request, LocalDateTime requestedAt) {
 		UrlShortCommand command = new UrlShortCommand();
 
 		command.original = request.getOriginal();
 		command.isExpirable = request.getIsExpirable();
+		command.expirationPeriod = request.getExpirationPeriod();
 		command.requestedAt = requestedAt;
-		command.expireAt = request.getExpirationPeriod().getExpireAt(requestedAt);
 
 		return command;
+	}
+
+	public LocalDateTime getExpireAt() {
+		return expirationPeriod.getExpireAt(requestedAt);
 	}
 }
